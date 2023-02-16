2a503ea3695df860aada6d13d36897606b07c54f
ssenr <duranr@protonmail.com>
2023-02-16 18:54:48 -0500

    Fixed some dependency issues

--- src/connector_init.cpp
+++ src/connector_init.cpp
@@ -1,29 +1,8 @@
 #include <connector.h>
 #include <config.h>
-#include <jdbc/mysql_connection.h>
-#include <jdbc/cppconn/driver.h>
-#include <jdbc/cppconn/exception.h>
-#include <jdbc/cppconn/resultset.h>
-#include <jdbc/cppconn/statement.h>
-#include <jdbc/cppconn/prepared_statement.h>
+#include <include/mysqlx/xdevapi.h>
 
 void conn_DB() 
 {
-    // sql::mysql::MySQL_Driver *driver;
-    // try
-    // {
-    //     sql::Connection *conn;
-    //     sql::Statement *stmt;
-    //     sql::ResultSet *res;
-    //     sql::PreparedStatement *pstmt;
-
-    //     /* Establish a Connection */
-    //     driver = get_driver_instance();
-    //     conn = driver->connect(CONN_STRING, USER_NAME, PASS_WORD);
-    //     std::cout << "Connected to database\n";
-    // }
-    // catch (int Age) 
-    // {
-    //     std::cout<<"Cannot connect to database\n";
-    // }
+    // Void
 }
