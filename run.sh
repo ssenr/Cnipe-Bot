a6b8fe56337cc6b0a2ccce62fc1ca23dfe2fcb64
ssenr <duranr@protonmail.com>
2023-02-18 21:36:30 -0500

    Added to Run Script

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 file_pth=./build/cnipe-bot
+dir_pth=./build
 
 if [ -x "$file_pth" ]; then
     ./build/cnipe-bot
+elif [ -d "$dir_pth" ]; then
+    cmake -B ./build
+    cmake --build ./build
+else    
+    mkdir ./build
+    cmake -B ./build
+    cmake --build ./build
 fi
