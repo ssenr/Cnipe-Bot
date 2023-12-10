8ffdf2e5a064c0643657f49cac8bde934ede5f4c
ssenr <duranr@protonmail.com>
2023-12-09 23:40:02 -0500

    Some refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -15,99 +15,99 @@
 
 namespace snipe
 {
-    inline std::optional<sqlite3pp::database> db_connect() 
+  inline std::optional<sqlite3pp::database> db_connect() 
+  {
+    try 
     {
-        try 
-        {
-            sqlite3pp::database db(SNIPE_DB_NAME);
-            return db;
-        }
-        catch (const std::exception&) { return std::nullopt;};
+      sqlite3pp::database db(SNIPE_DB_NAME);
+      return db;
     }
+    catch (const std::exception&) { return std::nullopt;};
+  }
 
-    /* Run Once */
-    void init()
-    {
-         
-    }
+  /* Run Once */
+  void init()
+  {
+
+  }
 }
 
 int main()
 {
-    /** INIT **/
-    // Cluster
-    const std::string BOT_TOKEN = BOT_TOKEN_H;
-    dpp::cluster bot(BOT_TOKEN, BOT_INTENTS);
+  /** INIT **/
+  // Cluster
+  const std::string BOT_TOKEN = BOT_TOKEN_H;
+  dpp::cluster bot(BOT_TOKEN, BOT_INTENTS);
 
-    // Database
-    std::optional<sqlite3pp::database> snipe_db_connect = snipe::db_connect();
-    if (!snipe_db_connect.has_value())
-    {
-        throw std::runtime_error("Database has not connected!");
-    }
-    sqlite3pp::database& snipe_db = snipe_db_connect.value(); 
-    /** INIT END **/
-    
-    const dpp::snowflake BOT_ID = bot.me.id; 
+  // Database
+  std::optional<sqlite3pp::database> snipe_db_connect = snipe::db_connect();
+  if (!snipe_db_connect.has_value())
+  {
+    throw std::runtime_error("Database has not connected!");
+  }
+  sqlite3pp::database& snipe_db = snipe_db_connect.value(); 
+  /** INIT END **/
 
-    dpp::slashcommand ping("ping", "Ping pong!", BOT_ID);
+  const dpp::snowflake BOT_ID = bot.me.id; 
 
-    std::vector<dpp::slashcommand> slashcommand_list = {ping};
+  dpp::slashcommand ping("ping", "Ping pong!", BOT_ID);
+
+  std::vector<dpp::slashcommand> slashcommand_list = {ping};
+
+  /**
+   *  Cache
+   **/
+  dpp::cache<dpp::message> message_cache;
+
+  const auto store = [&bot, &message_cache](const dpp::message_create_t& event)
+  {
+    dpp::message* event_message = new dpp::message();
+    *event_message = event.msg;
 
     /**
-     *  Cache
-    **/
-    dpp::cache<dpp::message> message_cache;
-
-    const auto store = [&bot, &message_cache](const dpp::message_create_t& event)
+     *  Check Channel ID 
+     *  Only one check for DEBUG purposes atm.
+     *  UPDATE TO CHANNEL ID VECTOR LATER (This way you can specify what channels to avoid)
+     **/
+    uint64_t channel_id = event_message->channel_id;
+    if (channel_id == GENERAL_CHANNEL_ID)
     {
-        dpp::message* event_message = new dpp::message();
-        *event_message = event.msg;
-
-        /**
-         *  Check Channel ID 
-         *  Only one check for DEBUG purposes atm.
-         *  UPDATE TO CHANNEL ID VECTOR LATER (This way you can specify what channels to avoid)
-         **/
-        uint64_t channel_id = event_message->channel_id;
-        if (channel_id == GENERAL_CHANNEL_ID)
-        {
-            message_cache.store(event_message);
-        }
-        else 
-        {
-            bot.log(dpp::loglevel::ll_critical, "Please type message in #general channel!");
-        }
-    };
-    bot.on_message_create(store);
-
-    // END
-    bot.on_log(dpp::utility::cout_logger());
-
-    bot.on_slashcommand([] (const dpp::slashcommand_t& event) 
+      message_cache.store(event_message);
+    }
+    else 
     {
-        if (event.command.get_command_name() == "ping") 
-        {
-            event.reply("Pong!");
-        }
-    });
+      bot.log(dpp::loglevel::ll_critical, "Please type message in #general channel!");
+    }
+  };
+  bot.on_message_create(store);
 
-    /** 
-     * INIT Function
-     * Handles:
-     * Database Status (Realistically if it reaches this point without a crash, and status is fine, can probably scan FS for .db file though)
-     * Slash Command Registration
-    **/
-    const auto init = [&](const dpp::ready_t& event) 
+  // END
+  bot.on_log(dpp::utility::cout_logger());
+
+  bot.on_slashcommand([] (const dpp::slashcommand_t& event) 
+      {
+      if (event.command.get_command_name() == "ping") 
+      {
+      event.reply("Pong!");
+      }
+      });
+
+  /** 
+   * INIT Function
+   * Handles:
+   * Database Status (Realistically if it reaches this point without a crash, and status is fine, can probably scan FS for .db file though)
+   * Slash Command Registration
+   **/
+  const auto init = [&](const dpp::ready_t& event) 
+  {
+    // Register Commands
+    if (dpp::run_once<struct register_bot_commands>()) 
     {
-        // Register Commands
-        if (dpp::run_once<struct register_bot_commands>()) 
-        {
-            bot.global_bulk_command_create(slashcommand_list);
-        }
-    };
+      bot.global_bulk_command_create(slashcommand_list);
+    }
+  };
 
-    bot.on_ready(init);
+  bot.on_ready(init);
 
-    bot.start(dpp::st_wait);
+  bot.start(dpp::st_wait);
 }
